eaa98fe97c5ce703008cf4e7d049ebd921526629
Ray Zhang <xiyunyue@gmail.com>
2018-06-18 13:16:11 -0700

    Add or replace with walmart license.

--- src/main/java/org/apache/hadoop/fs/swifta/snative/LFUCache.java
+++ src/main/java/org/apache/hadoop/fs/swifta/snative/LFUCache.java
@@ -10,9 +10,22 @@ public class LFUCache<T> {
 
   private static final Log LOG = LogFactory.getLog(LFUCache.class);
 
-  /**
-   * Store node position in doubly linked list [key, Node].
-   */
+  /*
+ * Copyright (c) [2011]-present, Walmart Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
   private Map<String, LFUNode> positionNodes = new HashMap<String, LFUNode>();
 
   /**
